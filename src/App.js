6ccc0f075b4f10be2887a2e4c53795ea38b238e7
Soham Das <soham.das.asm@gmail.com>
2023-01-18 20:04:17 -0800

    Hello!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { WEATHER_API } from './constants'; 
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const handleSearch = evt => {
+    if (evt.key === "Enter") {
+      fetchWeather();
+    }
+  }
+
+  const fetchWeather = async () => {
+    try {
+      const response = await fetch(`${WEATHER_API.base}weather?q=${query}&units=imperial&APPID=${WEATHER_API.key}`);
+      const data = await response.json();
+      setWeather(data);
+      setQuery('');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const weatherClassName = () => {
+    if (typeof weather.main === "undefined") {
+      return 'app';
+    }
+    switch (weather.weather[0].main) {
+      case 'Clouds':
+        return 'appcloudy';
+      case 'Snow':
+        return 'appsnowy';
+      case 'Rain':
+        return 'apprainy';
+      case 'Clear':
+        return 'app';
+      default:
+        return 'app';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={weatherClassName()}>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={handleSearch}
+          />
+        </div>
+        {(typeof weather.main !== "undefined") && (
+          <div>
+            <div className="location-box">
+              <div className="location">{weather.name}, {weather.sys.country}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">
+                {Math.round(weather.main.temp)}°F
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        )}
+      </main>
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+const WEATHER_API = {
+    key: "84f8f016a40d78f21b85a71cdcff7dd5",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
+export {WEATHER_API};
